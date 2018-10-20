7a746b58c149a5f9977a7b882e734a10d0717711
Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>
2018-10-20 18:18:44 +0700

    GCC 8.2.1 Bare Metal Elf

---
+++ lib/gcc/aarch64_be-elf/8.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/tmp/dgboter/bbs/bc-b1-2-12--rhe6x86_64/buildbot/rhe6x86_64--aarch64_be-elf/build/src/gcc/configure --target=aarch64_be-elf --prefix=/tmp/dgboter/bbs/bc-b1-2-12--rhe6x86_64/buildbot/rhe6x86_64--aarch64_be-elf/build/build-aarch64_be-elf/install// --with-gmp=/tmp/dgboter/bbs/bc-b1-2-12--rhe6x86_64/buildbot/rhe6x86_64--aarch64_be-elf/build/build-aarch64_be-elf/host-tools --with-mpfr=/tmp/dgboter/bbs/bc-b1-2-12--rhe6x86_64/buildbot/rhe6x86_64--aarch64_be-elf/build/build-aarch64_be-elf/host-tools --with-mpc=/tmp/dgboter/bbs/bc-b1-2-12--rhe6x86_64/buildbot/rhe6x86_64--aarch64_be-elf/build/build-aarch64_be-elf/host-tools --with-isl=/tmp/dgboter/bbs/bc-b1-2-12--rhe6x86_64/buildbot/rhe6x86_64--aarch64_be-elf/build/build-aarch64_be-elf/host-tools --with-pkgversion='GNU Toolchain for the A-profile Architecture 8.2-2018-08 (arm-rel-8.23)' --disable-shared --disable-nls --disable-threads --disable-tls --enable-checking=release --enable-languages=c,c++,fortran --with-newlib";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
